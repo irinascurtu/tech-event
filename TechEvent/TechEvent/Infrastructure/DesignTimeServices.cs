f9d933bd3a6bce9285278104f770612b1a4fbbc2
Irina Scurtu <irina.scurtu@fortech.ro>
2019-02-16 12:11:18 +0200

    singularized entities

--- TechEvent/TechEvent/Infrastructure/DesignTimeServices.cs
+++ TechEvent/TechEvent/Infrastructure/DesignTimeServices.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore.Design;
+﻿using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace TechEvent.Web.Infrastructure
